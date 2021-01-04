c6d0202d6ec666a3c9c5b943bdd83085cabaf609
Sang Hyup Lee <tms.lee9@gmail.com>
2021-01-04 19:32:20 +0000

    wrote assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  let actualOut = typeof actual === 'string' ? `"${actual}"` : actual;
+  let expectedOut = typeof expected === 'string' ? `"${expected}"` : expected;
+  if (actual === expected) console.log(`👌👌👌 Assertion Passed: ${actualOut} === ${expectedOut}`);
+  else console.log(`🙅‍♀️🙅‍♀️🙅‍♀️ Assertion Failed: ${actualOut} !== ${expectedOut}`);
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("aaa", "aaa");
+assertEqual(1,10);
+assertEqual(1, "1");
+
