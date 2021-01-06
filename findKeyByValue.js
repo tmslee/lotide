e6b21f96da9fed30f26be373ea714aba0b5cbf35
Sang Hyup Lee <tms.lee9@gmail.com>
2021-01-06 18:56:11 +0000

    wrote findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  let actualOut = typeof actual === 'string' ? `"${actual}"` : actual;
+  let expectedOut = typeof expected === 'string' ? `"${expected}"` : expected;
+  if (actual === expected) console.log(`👌👌👌 Assertion Passed: ${actualOut} === ${expectedOut}`);
+  else console.log(`🙅‍♀️🙅‍♀️🙅‍♀️ Assertion Failed: ${actualOut} !== ${expectedOut}`);
+};
+
+const findKeyByValue = function(obj, val){
+  for (let key in obj) {
+    if(obj[key] === val) return key;
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire",
+  anotherGenre : "The Expanse",
+  "": "edgeCase1",
+  "edgeCase2":"" 
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "edgeCase1"), "");
+assertEqual(findKeyByValue(bestTVShowsByGenre, ""), "edgeCase2");
