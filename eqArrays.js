649c4e5fd388873f8847f01aa676cd415d93ac06
Sang Hyup Lee <tms.lee9@gmail.com>
2021-01-07 20:28:13 +0000

    updated eqArrays function to be recursive

--- eqArrays.js
+++ eqArrays.js
@@ -9,7 +9,10 @@ const eqArrays = (arr1, arr2) => {
   if (arr1.length !== arr2.length) return false;
   else {
     for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) return false;
+      if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+        if(!eqArrays(arr1[i], arr2[i])) return false;
+      }
+      else if (arr1[i] !== arr2[i]) return false;
     }
     return true;
   }
@@ -22,3 +25,7 @@ assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
 assertEqual(eqArrays([1, 2, 3], [0, 2, 3]), false);
 assertEqual(eqArrays([1, 2, 3], ['1', '2', '3']), false);
 assertEqual(eqArrays([1, 2, 3], '[1, 2, 3]'), false);
+
+assertEqual(eqArrays([1, 2, 3, [4,5,6], [[7,8,9],[10],[11,12], 13, 14]], [1, 2, 3, [4,5,6], [[7,8,9],[10],[11,12], 13, 14]]), true);
+assertEqual(eqArrays([1, 2, 3, [4,5,6], [[7,8,9],[10],[11,12], 13, 14]], [1, 2, 3, [4,5,6], [[7,8,500],[10],[11,12], 13, 14]]), false);
+
