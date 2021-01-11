6ea32622bd1e89c71c486dc9092e1aa59a6867fd
Sang Hyup Lee <tms.lee9@gmail.com>
2021-01-11 19:04:58 +0000

    refactored middle and wrote test file

--- middle.js
+++ middle.js
@@ -1,18 +1,3 @@
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) return false;
-  else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) return false;
-    }
-    return true;
-  }
-};
-
-const assertArraysEqual = function(arr1, arr2){
-  let message = eqArrays(arr1, arr2) ? `👌👌👌 Assertion Passed: ${arr1} === ${arr2}`:`🙅‍♀️🙅‍♀️🙅‍♀️ Assertion Failed: ${arr1} !== ${arr2}`;
-  console.log(message);
-}
-
 const middle = function(arr) {
   if(arr.length  < 3) return [];
   else{
@@ -22,9 +7,4 @@ const middle = function(arr) {
   }
 }
 
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1,2]), []);
-assertArraysEqual(middle([1,2,3]), [2]);
-assertArraysEqual(middle([1,2,3,4]), [2,3]);
-assertArraysEqual(middle(['a','b','c','d','e','f']), ['c', 'd']);
+module.exports = middle;
